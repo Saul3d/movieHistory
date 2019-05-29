1308dbda3653be6394263be49736f561ef5242c2
Saul Solano <saul3d@yahoo.com>
2019-05-28 21:32:48 -0500

    authentication added

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,3 +1,19 @@
+import firebase from 'firebase/app';
+import Auth from './components/auth/auth';
+import movies from './components/movies/movies';
+import apiKeys from './helpers/apiKeys.json';
+import myNavbar from './components/myNavbar/myNavbar';
+
+import authData from './helpers/data/authData';
+
 import '../styles/main.scss';
 
-console.error('hi');
+const init = () => {
+  firebase.initializeApp(apiKeys.firebaseKeys);
+  myNavbar.navbarEvents();
+  authData.checkLoginStatus();
+  Auth.authStringBuilder();
+  movies.moviesStringBuilder();
+};
+
+init();
